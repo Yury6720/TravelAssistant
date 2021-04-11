3f8d5d1a06c20b76f715dfa17bb0cd13cdd71176
Alex Flyer <54953161+Flyer-jr@users.noreply.github.com>
2021-04-11 19:51:14 +0300

    Initial Commit

---
+++ eureka-discovery/src/main/java/com/travel/eurekadiscovery/DiscoveryServiceApplication.java
@@ -0,0 +1,15 @@
+package com.travel.eurekadiscovery;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@SpringBootApplication
+@EnableEurekaServer
+public class DiscoveryServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DiscoveryServiceApplication.class, args);
+	}
+
+}
